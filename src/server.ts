94d47fe1fcbbefada0a5d795ddf8699b24b74085
Rupom <saidurrahmanrupom@gmail.com>
2026-07-03 18:44:09 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
+
+const PORT = config.port;
+
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+main();
