2eec13995ace885bf7b93660fe91f92bdfeb9db7
Rupom <saidurrahmanrupom@gmail.com>
2026-07-05 00:13:27 +0600

    Defining Catch Async As Higher Order Function

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,32 +1,48 @@
 import httpStatus from "http-status";
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { userService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
+// const registerUser = async (req: Request, res: Response) => {
+//   try {
+//     const payload = req.body;
+
+//     const user = await userService.registerUserintoDB(payload);
+
+//     res.status(httpStatus.CREATED).json({
+//       success: true,
+//       statuCode: httpStatus.CREATED,
+
+//       message: "User created successfully",
+//       data: {
+//         user,
+//       },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//       success: false,
+//       statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//       message: "Failed to create user",
+//       error: (error as Error).message,
+//     });
+//   }
+// };
+
+const registerUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-
     const user = await userService.registerUserintoDB(payload);
-
     res.status(httpStatus.CREATED).json({
       success: true,
       statuCode: httpStatus.CREATED,
-
       message: "User created successfully",
       data: {
         user,
       },
     });
-  } catch (error) {
-    console.log(error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to create user",
-      error: (error as Error).message,
-    });
-  }
-};
+  },
+);
 
 export const userController = {
   registerUser,
