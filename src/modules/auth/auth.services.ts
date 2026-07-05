1326b33f9010de02bc1129b4b589fbea4f669842
Rupom <saidurrahmanrupom@gmail.com>
2026-07-05 18:56:52 +0600

    Implementing Jwt For Access Token And Refresh Token

--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { IloginUser } from "./auth.interface";
+import Jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const loginUser = async (payload: IloginUser) => {
   const { email, password } = payload;
@@ -16,7 +19,34 @@ const loginUser = async (payload: IloginUser) => {
     throw new Error("Password does not match");
   }
 
-  return user;
+  const jwtpayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  //   const accesstoken = Jwt.sign(jwtpayload, config.jwt_access_secret, {
+  //     expiresIn: config.jwt_access_expires_in,
+  //   } as SignOptions);
+
+  const accesstoken = jwtUtils.createToken(
+    jwtpayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  //   const refreshtoken = Jwt.sign(jwtpayload, config.jwt_refresh_secret, {
+  //     expiresIn: config.jwt_refresh_expires_in,
+  //   } as SignOptions);
+
+  const refreshtoken = jwtUtils.createToken(
+    jwtpayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
+  return { accesstoken, refreshtoken };
 };
 
 export const authService = {
